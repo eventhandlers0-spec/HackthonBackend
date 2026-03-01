a7bf4d715f466f5bcde41fcc52bb9e02dd9bea05
eventhandlers0-spec <eventhandlers0@gmail.com>
2026-03-01 12:37:36 +0530

    Update GlobalConnectors.java

--- backend/demo/src/main/java/com/example/EventHandler/GlobalConnectors.java
+++ backend/demo/src/main/java/com/example/EventHandler/GlobalConnectors.java
@@ -1,5 +1,5 @@
 
-package com.example.demo;
+package com.example.EventHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -24,6 +24,7 @@ public class GlobalConnectors {
         return source;
     }
 }
+
 
 
 
